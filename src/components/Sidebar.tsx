786f02adb093467ec70d41ea5116150bc35aea01
ivandelimaw3 <maxtopcon@gmail.com>
2025-07-19 18:16:44 -0300

    Add files via upload

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -22,6 +22,7 @@ export const Sidebar: React.FC = () => {
     { icon: LayoutDashboard, label: 'Painel de Negócios', path: '/' },
     { icon: CreditCard, label: 'Contas', path: '/contas' },
     { icon: FolderOpen, label: 'Categorias', path: '/categorias' },
+    { icon: CreditCard, label: 'Cartões', path: '/cartoes' },
     { icon: TrendingUp, label: 'Relatórios', path: '/relatorios' },
     { icon: Building2, label: 'Bancos', path: '/bancos' },
     { icon: PieChart, label: 'Investimentos', path: '/investimentos' },
